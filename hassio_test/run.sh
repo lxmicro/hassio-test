015df98ce344570f8aa002176e38348f03bbf499
lxmicro <alvaro.ramos.soltero@gmail.com>
2020-05-18 16:21:51 -0500

    Update run.sh

--- hassio_test/run.sh
+++ hassio_test/run.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 
 set -e
 
 INTERFACE="dummy0"
 IP="10.0.0.1"
+CONFIG="/etc/dhcpd.conf"
+LEASES="/data/dhcpd.lease"
 
 INI_CMDS_1="ifconfig $INTERFACE | awk '{ if(match(\$0,/$INTERFACE/)){ print 0; } }'"
 INI_CMDS_2="ip link delete $INTERFACE 2>/dev/null"
@@ -13,8 +15,8 @@ STR_CMDS_2="ip link set $INTERFACE multicast on 2>/dev/null"
 STR_CMDS_3="ip addr add $IP/24 dev $INTERFACE 2>/dev/null"
 STR_CMDS_4="ip link set $INTERFACE up 2>/dev/null"
 
-term_handler(){
-  echo "Stopping..."
+stop_addon(){
+  bashio::log.info "Removing Network Interface ..."
   local cmd=$(echo "$INI_CMDS_2")
   local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
   if [ "$r" == 0 ]; then
@@ -26,12 +28,14 @@ term_handler(){
   fi
 }
 
-function cmd_init {
+function cmd_init() {
   local r=$(eval "$INI_CMDS_1")
   if [ "$r" == 0 ]; then
+    bashio::log.info "$INTERFACE exists, try to remove ..."
     local cmd=$(echo "$INI_CMDS_2")
     local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
     if [ "$r" == 0 ]; then
+      bashio::log.info "Ok: $INTERFACE removed ..."
       echo 0
       return 0
     fi
@@ -66,15 +70,18 @@ function create_interface(){
   return 1
 }
 
-trap 'term_handler' SIGTERM
+trap "stop_addon" SIGTERM SIGHUP
+bashio::log.info "Starting ..."
 
 if [ "$(cmd_init)" -ne 0 ]; then
+  bashio::log.info "Error: interface exists"
   exit 1
 fi
 
 if [ "$(create_interface)" -ne 0 ]; then
+  bashio::log.info "Error: unable to start interface"
   exit 1
 fi
 
-echo "finalizado"
+bashio::log.info "Ok"
 exit 0
