68b33133fe1549be469f63b103660c8477db250b
Alvaro Ramos <alvaro.ramos.soltero@gmail.com>
2020-05-18 00:56:29 -0500

    Update run.sh

--- hassio_test/run.sh
+++ hassio_test/run.sh
@@ -1,3 +1,66 @@
 #!/bin/bash
 
-python3 -m http.server 8000
+set -e
+
+INTERFACE="dummy0"
+IP="10.0.0.0"
+
+INI_CMDS_1="ifconfig $INTERFACE | awk '{ if(match(\$0,/$INTERFACE/)){ print 0; } }'"
+INI_CMDS_2="ip link delete $INTERFACE 2>/dev/null"
+
+STR_CMDS_1="ip link add $INTERFACE type dummy 2>/dev/null"
+STR_CMDS_2="ip link set $INTERFACE multicast on 2>/dev/null"
+STR_CMDS_3="ip addr add $IP/24 dev $INTERFACE 2>/dev/null"
+STR_CMDS_4="ip link set $INTERFACE up 2>/dev/null"
+
+
+function cmd_init {
+  local r=$(eval "$INI_CMDS_1")
+  if [ "$r" == 0 ]; then
+    local cmd=$(echo "$INI_CMDS_2")
+    local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
+    if [ "$r" == 0 ]; then
+      echo 0
+      return 0
+    fi
+  else
+    echo 0
+    return 0
+  fi
+  echo 1
+  return 1
+}
+
+function create_interface(){
+  local cmd=$(echo "$STR_CMDS_1")
+  local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
+  if [ "$r" == 0 ]; then
+    local cmd=$(echo "$STR_CMDS_2")
+    local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
+    if [ "$r" == 0 ]; then
+      local cmd=$(echo "$STR_CMDS_3")
+      local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
+      if [ "$r" == 0 ]; then
+        local cmd=$(echo "$STR_CMDS_4")
+        local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
+        if [ "$r" == 0 ]; then
+          echo 0
+          return 0
+        fi
+      fi
+    fi
+  fi
+  echo 1
+  return 1
+}
+
+if [ "$(cmd_init)" -ne 0 ]; then
+  exit 1
+fi
+
+if [ "$(create_interface)" -ne 0 ]; then
+  exit 1
+fi
+
+echo "finalizado"
+exit 0
