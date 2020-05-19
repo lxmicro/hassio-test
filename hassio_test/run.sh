56bd8cb2cf39d053a257256b8c22d6e1d8aa4bdd
lxmicro <alvaro.ramos.soltero@gmail.com>
2020-05-18 22:09:43 -0500

    Update run.sh

--- hassio_test/run.sh
+++ hassio_test/run.sh
@@ -15,7 +15,7 @@ STR_CMDS_2="ip link set $INTERFACE multicast on 2>/dev/null"
 STR_CMDS_3="ip addr add $IP/24 dev $INTERFACE 2>/dev/null"
 STR_CMDS_4="ip link set $INTERFACE up 2>/dev/null"
 
-stop_addon(){
+function stop_addon(){
   bashio::log.info "Removing Network Interface ..."
   local cmd=$(echo "$INI_CMDS_2")
   local r=$(awk -v cmd="$cmd" 'BEGIN {rst=system(cmd); print rst}')
@@ -70,6 +70,63 @@ function create_interface(){
   return 1
 }
 
+function TABLE_UP_RULE_01(){
+  local cmd=$(iptables-save | grep -- "-A POSTROUTING -o eth0 -j MASQUERADE" | awk '{ if ( $0 ){ print "1" } }')
+  if [ ! -z "$cmd" ]; then
+     echo 0
+     return 0
+  else
+     su -s /bin/sh -c "/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" root
+     if [ "$?" -ne 0 ]; then
+       echo 1
+       return 1
+     else
+       echo 0
+       return 0
+     fi
+  fi
+  echo 1
+  return 1
+}
+
+function TABLE_UP_RULE_02(){
+  local cmd=$(iptables-save | grep -- "-A FORWARD -i eth0 -o $INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT" | awk '{ if ( $0 ){ print "1" } }')
+  if [ ! -z "$cmd" ]; then
+     echo 0
+     return 0
+  else
+     su -s /bin/sh -c "/sbin/iptables -A FORWARD -i eth0 -o $INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT" root
+     if [ "$?" -ne 0 ]; then
+       echo 1
+       return 1
+     else
+       echo 0
+       return 0
+     fi
+  fi
+  echo 1
+  return 1
+}
+
+function TABLE_UP_RULE_03(){
+  local cmd=$(iptables-save | grep -- "-A FORWARD -i $INTERFACE -o eth0 -j ACCEPT" | awk '{ if ( $0 ){ print "1" } }')
+  if [ ! -z "$cmd" ]; then
+     echo 0
+     return 0
+  else
+     su -s /bin/sh -c "/sbin/iptables -A FORWARD -i $INTERFACE -o eth0 -j ACCEPT" root
+     if [ "$?" -ne 0 ]; then
+       echo 1
+       return 1
+     else
+       echo 0
+       return 0
+     fi
+  fi
+  echo 1
+  return 1
+}
+
 trap "stop_addon" SIGTERM SIGHUP
 bashio::log.info "Starting ..."
 
@@ -80,6 +137,23 @@ fi
 
 if [ "$(create_interface)" -ne 0 ]; then
   bashio::log.info "Error: unable to start interface"
+  exit 1
+fi
+
+bashio::log.info "Creating iptables rules ..."
+
+if [ "$(TABLE_UP_RULE_01)" -ne 0 ]; then
+  bashio::log.info "Error iptables rule 1"
+  exit 1
+fi
+
+if [ "$(TABLE_UP_RULE_02)" -ne 0 ]; then
+  bashio::log.info "Error iptables rule 2"
+  exit 1
+fi
+
+if [ "$(TABLE_UP_RULE_03)" -ne 0 ]; then
+  bashio::log.info "Error iptables rule 3"
   exit 1
 fi
 
